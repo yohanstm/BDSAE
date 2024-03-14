23fdab4008086a97c898d14d21558a59662e3709
yohanstm <143810458+yohanstm@users.noreply.github.com>
2024-03-14 15:38:39 +0100

    Update SQLYohan.sql

--- sae_bd/SQLYohan.sql
+++ sae_bd/SQLYohan.sql
@@ -1,3 +1,80 @@
+-- Devoir 211
+-- Nom: SAINT MARC , Prenom: Yohan
+
+-- Feuille SAE2.04 Exploitation d'une base de donn�es
+-- 
+-- Veillez � bien r�pondre aux emplacements indiqu�s.
+-- Seule la premi�re requ�te est prise en compte.
+
+-- +-----------------------+--
+-- * Question 211442 : 2pts --
+-- +-----------------------+--
+-- Ecrire une requ�te qui renvoie les informations suivantes:
+--  Quelles sont les formations offertes par l'IUT d'Orl�ans en 2023? On veut le num�ro de formation, la fili�re, le nom (fil_lib_voe_acc)
+
+-- Voici le d�but de ce que vous devez obtenir.
+-- ATTENTION � l'ordre des colonnes et leur nom!
+-- +----------+----------------------------------------------------------+---------------------+----------+-------------------+
+-- | num_form | fil_lib_voe_acc                                          | select_form         | num_fili | fili              |
+-- +----------+----------------------------------------------------------+---------------------+----------+-------------------+
+-- | etc...
+-- = Reponse question 211442.
+select num_form, fil_lib_voe_acc, select_form, num_fili, fili 
+from FORMATION natural join FILIERE natural join VOEUX natural join ETABLISSEMENT natural join STATS 
+where session =2023 and g_ea_lib_vx = "I.U.T d'Orléans" and fili = 'BUT';
+
+
+
+-- +-----------------------+--
+-- * Question 211475 : 2pts --
+-- +-----------------------+--
+-- Ecrire une requ�te qui renvoie les informations suivantes:
+--  Quels sont les �tablissements de la Creuse qui offrent un BTS? On veut le code uai, le nom de l'�tablissement ( g_ea_lib_vx) et la ville.
+
+-- Voici le d�but de ce que vous devez obtenir.
+-- ATTENTION � l'ordre des colonnes et leur nom!
+-- +----------+----------------------------------+------------+------+
+-- | cod_uai  | g_ea_lib_vx                      | ville_etab | FILI |
+-- +----------+----------------------------------+------------+------+
+-- | etc...
+-- = Reponse question 211475.
+select distinct cod_uai, g_ea_lib_vx, ville_etab, fili
+from ETABLISSEMENT natural join DEPARTEMENT natural join FORMATION natural join FILIERE
+where dep_lib ='Creuse' and fili ='BTS';
+
+
+-- +-----------------------+--
+-- * Question 211486 : 2pts --
+-- +-----------------------+--
+-- Ecrire une requ�te qui renvoie les informations suivantes:
+--  Combien de candidats ont �t� class�s lors de la phase principale de 2023 par les diff�rents �tablissements qui d�livrent le BUT QLIO? On veut le code uai, le nom et la ville de l'�tablissement et le nombre de class�s lors de la phase principale (nb_cla_pp)
+
+-- Voici le d�but de ce que vous devez obtenir.
+-- ATTENTION � l'ordre des colonnes et leur nom!
+-- +----------+-----------------------------------------------------+--------------------+-----------+
+-- | cod_uai  | g_ea_lib_vx                                         | ville_etab         | nb_cla_pp |
+-- +----------+-----------------------------------------------------+--------------------+-----------+
+-- | etc...
+-- = Reponse question 211486.
+SELECT cod_uai, g_ea_lib_vx, ville_etab, nb_cla_pp from ETABLISSEMENT natural join VOEUX natural join FORMATION natural join STATS
+where session = 2023 and  fil_lib_voe_acc = 'Qualité, logistique industrielle et organisation' ;
+
+
+-- * Question 211521 : 2pts --
+-- +-----------------------+--
+-- Ecrire une requ�te qui renvoie les informations suivantes:
+--  La liste des lyc�es agricoles publics de la Gironde
+
+-- Voici le d�but de ce que vous devez obtenir.
+-- ATTENTION � l'ordre des colonnes et leur nom!
+-- +----------+----------------------------------------+-------------+
+-- | cod_uai  | g_ea_lib_vx                            | ville_etab  |
+-- +----------+----------------------------------------+-------------+
+-- | etc...
+-- = Reponse question 211521.
+SELECT cod_uai, g_ea_lib_vx, ville_etab from ETABLISSEMENT natural join DEPARTEMENT
+where dep_lib = 'Gironde' and contrat_etab = 'Public' LIKE '%agricole';
+
 -- +-----------------------+--
 -- * Question 211565 : 2pts --
 -- +-----------------------+--
