07853ed3de8d6f243b84c76caf81c2053f79f42f
yohanstm <143810458+yohanstm@users.noreply.github.com>
2024-03-14 15:36:29 +0100

    Create SQLYohan.sql

---
+++ sae_bd/SQLYohan.sql
@@ -0,0 +1,199 @@
+-- +-----------------------+--
+-- * Question 211565 : 2pts --
+-- +-----------------------+--
+-- Ecrire une requ�te qui renvoie les informations suivantes:
+--  Donner la liste des d�partements dans lesquels il n'y a aucune offre de Licence
+
+-- Voici le d�but de ce que vous devez obtenir.
+-- ATTENTION � l'ordre des colonnes et leur nom!
+-- +-----+-------------------------+
+-- | dep | dep_lib                 |
+-- +-----+-------------------------+
+-- | etc...
+-- = Reponse question 211565.
+SELECT DEPARTEMENT.dep, DEPARTEMENT.dep_lib
+FROM DEPARTEMENT
+WHERE NOT EXISTS (
+    SELECT *
+    FROM FORMATION
+    WHERE FORMATION.dep = DEPARTEMENT.dep AND fili = 'Licence'
+);
+
+
+-- +-----------------------+--
+-- * Question 211576 : 2pts --
+-- +-----------------------+--
+-- Ecrire une requ�te qui renvoie les informations suivantes:
+--  Donner la liste des �tablissements qui offrent � la fois une licence de Droit et une licence informatique
+
+-- Voici le d�but de ce que vous devez obtenir.
+-- ATTENTION � l'ordre des colonnes et leur nom!
+-- +----------+--------------------------------------------------------------+----------------+
+-- | cod_uai  | g_ea_lib_vx                                                  | ville_etab     |
+-- +----------+--------------------------------------------------------------+----------------+
+-- | etc...
+-- = Reponse question 211576.
+SELECT DISTINCT E1.cod_uai, E1.g_ea_lib_vx, E1.ville_etab
+FROM ETABLISSEMENT E1
+JOIN FORMATION F1 ON E1.cod_uai = F1.cod_uai AND F1.fili = 'Droit'
+JOIN ETABLISSEMENT E2 ON E1.cod_uai = E2.cod_uai
+JOIN FORMATION F2 ON E2.cod_uai = F2.cod_uai AND F2.fili = 'Informatique';
+
+
+-- +-----------------------+--
+-- * Question 211622 : 2pts --
+-- +-----------------------+--
+-- Ecrire une requ�te qui renvoie les informations suivantes:
+--  Donner la liste des �tablissements de l'acad�mie de Versailles qui n'offrent que des Licences
+
+-- Voici le d�but de ce que vous devez obtenir.
+-- ATTENTION � l'ordre des colonnes et leur nom!
+-- +----------+---------------------------------------------------------------------------------+---------------------+
+-- | cod_uai  | g_ea_lib_vx                                                                     | ville_etab          |
+-- +----------+---------------------------------------------------------------------------------+---------------------+
+-- | etc...
+-- = Reponse question 211622.
+SELECT E1.cod_uai, E1.g_ea_lib_vx, E1.ville_etab
+FROM ETABLISSEMENT E1
+JOIN ACADEMIE A ON E1.cod_aca = A.cod_aca AND A.aca_lib = 'Versailles'
+WHERE NOT EXISTS (
+    SELECT *
+    FROM FORMATION F
+    WHERE F.cod_uai = E1.cod_uai AND F.fili != 'Licence'
+);
+
+
+-- +-----------------------+--
+-- * Question 211677 : 2pts --
+-- +-----------------------+--
+-- Ecrire une requ�te qui renvoie les informations suivantes:
+--  Donner le nombre d'�tablissements publics par acad�mie
+
+-- Voici le d�but de ce que vous devez obtenir.
+-- ATTENTION � l'ordre des colonnes et leur nom!
+-- +---------------------+-------+
+-- | acad_mies           | nb_et |
+-- +---------------------+-------+
+-- | etc...
+-- = Reponse question 211677.
+SELECT A.aca_lib AS acad_mies, COUNT(E.cod_uai) AS nb_et
+FROM ACADEMIE A
+LEFT JOIN ETABLISSEMENT E ON A.cod_aca = E.cod_aca AND E.contrat_etab = 'Public'
+GROUP BY A.aca_lib;
+
+
+-- +-----------------------+--
+-- * Question 211712 : 2pts --
+-- +-----------------------+--
+-- Ecrire une requ�te qui renvoie les informations suivantes:
+--  Donner le nombre total de candidatures en licence en 2023 par r�gion
+
+-- Voici le d�but de ce que vous devez obtenir.
+-- ATTENTION � l'ordre des colonnes et leur nom!
+-- +----------------------------+------------+
+-- | region_etab_aff            | nb_voe_but |
+-- +----------------------------+------------+
+-- | etc...
+-- = Reponse question 211712.
+SELECT R.region_etab_aff, COUNT(V.cod_voe) AS nb_voe_but
+FROM REGION R
+LEFT JOIN ETABLISSEMENT E ON R.cod_reg = E.cod_reg
+LEFT JOIN VOEUX V ON E.cod_uai = V.cod_uai AND V.session = 2023 AND V.select_form = 'BUT'
+WHERE E.fili = 'Licence'
+GROUP BY R.region_etab_aff;
+
+
+-- +-----------------------+--
+-- * Question 211756 : 2pts --
+-- +-----------------------+--
+-- Ecrire une requ�te qui renvoie les informations suivantes:
+--  Donner la ou les formations qui ont re�u le plus de voeux en phase principale en 2023
+
+-- Voici le d�but de ce que vous devez obtenir.
+-- ATTENTION � l'ordre des colonnes et leur nom!
+-- +----------+-------------+--------------+-------------------+-----------+
+-- | cod_uai  | g_ea_lib_vx | ville_etab   | fili              | nb_voe_pp |
+-- +----------+-------------+--------------+-------------------+-----------+
+-- | etc...
+-- = Reponse question 211756.
+SELECT E.cod_uai, E.g_ea_lib_vx, E.ville_etab, F.fili, COUNT(V.cod_voe) AS nb_voe_pp
+FROM ETABLISSEMENT E
+JOIN FORMATION F ON E.cod_uai = F.cod_uai
+JOIN VOEUX V ON F.num_form = V.num_form AND V.session = 2023 AND V.select_form = 'BUT'
+WHERE V.phase_voeu = 'Principale'
+GROUP BY E.cod_uai, F.fili
+ORDER BY nb_voe_pp DESC
+LIMIT 1;
+
+
+-- +-----------------------+--
+-- * Question 211802 : 2pts --
+-- +-----------------------+--
+-- Ecrire une requ�te qui renvoie les informations suivantes:
+--  Donner la liste des acad�mies qui ont offert plus de places en 2022 qu'en 2023
+
+-- Voici le d�but de ce que vous devez obtenir.
+-- ATTENTION � l'ordre des colonnes et leur nom!
+-- +---------------------+
+-- | acad_mies           |
+-- +---------------------+
+-- | etc...
+-- = Reponse question 211802.
+SELECT A.aca_lib
+FROM ACADEMIE A
+JOIN ETABLISSEMENT E ON
+-- +---------------------+
+-- | acad_mies           |
+-- +---------------------+
+-- | etc...
+-- = Reponse question 211802.
+SELECT A.aca_lib
+FROM ACADEMIE A
+JOIN ETABLISSEMENT E ON A.cod_aca = E.cod_aca
+JOIN FORMATION F ON E.cod_uai = F.cod_uai
+WHERE F.session = 2022
+GROUP BY A.aca_lib
+HAVING SUM(F.capacite_form) > (
+    SELECT SUM(F2.capacite_form)
+    FROM FORMATION F2
+    WHERE F2.session = 2023
+);
+
+
+-- +-----------------------+--
+-- * Question 211857 : 2pts --
+-- +-----------------------+--
+-- Ecrire une requ�te qui renvoie les informations suivantes:
+--  Requ�te pour le 1er graphique
+
+-- Voici le d�but de ce que vous devez obtenir.
+-- ATTENTION � l'ordre des colonnes et leur nom!
+-- +-------------------+---------+
+-- | fili              | nb_form |
+-- +-------------------+---------+
+-- | etc...
+-- = Reponse question 211857.
+SELECT F.fili, COUNT(F.num_form) AS nb_form
+FROM FORMATION F
+GROUP BY F.fili
+ORDER BY nb_form DESC;
+
+
+-- +-----------------------+--
+-- * Question 211879 : 2pts --
+-- +-----------------------+--
+-- Ecrire une requ�te qui renvoie les informations suivantes:
+--  Requ�te pour le 2eme graphique
+
+-- Voici le d�but de ce que vous devez obtenir.
+-- ATTENTION � l'ordre des colonnes et leur nom!
+-- +-------------------+----------+
+-- | filiere           | nb_voeux |
+-- +-------------------+----------+
+-- | etc...
+-- = Reponse question 211879.
+SELECT F.filiere, COUNT(V.cod_voe) AS nb_voeux
+FROM VOEUX V
+JOIN FORMATION F ON V.num_form = F.num_form
+GROUP BY F.filiere
+ORDER BY nb_voeux DESC;
